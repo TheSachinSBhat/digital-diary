deb7fff7593232821762c731d00dd35f06b36a5a
Sachin Bhat <bhat.sachinsbhat@gmail.com>
2017-04-01 22:41:04 +0530

    Commit #177: Fix the view name for ideas route

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^dashboard', hello.views.dashboard, name='dashboard'),
-    url(r'^ideas', hello.views.dashboard, name='ideas'),
+    url(r'^ideas', hello.views.ideas, name='ideas'),
     url(r'^admin/', include(admin.site.urls)),
 ]
